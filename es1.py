dbebb2ea642fbf3d35a14ff898c24fe952e21776
MarcoGiorgis <marco.giorgis.898@itiscuneo.eu>
2023-09-22 11:09:08 +0200

    commit di prova

--- es1.py
+++ es1.py
@@ -1,4 +1,11 @@
-print("ciao mondo")
-# questo è un commento
-nome = "luca"
-print(f"Ciao {nome}") #f string
+def main(): #definizione funzione main
+    nome = input("come ti chiami?")
+    anni = int(input("quanti anni hai?")) #input ritorna solo strighe
+    anno_corrente = int(input("in quale hanno siamo? "))
+    print(f"il tuo nome è {nome} e hai {anni} anni") #si mette f con stringa variabile
+    print(f"se nato nel {anno_corrente-anni}")
+    print("ciao") #la print va a capo da sola
+    print(type(anni))
+
+if __name__ == "__main__": #per eseguire il main
+    main()
